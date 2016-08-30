853ec15aabdc8e80dd5f6d33b24df06a629b90f4
torabi-ali <alitorabi2020@gmail.com>
2016-08-30 12:56:47 +0430

    adding veiws

--- download/urls.py
+++ download/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'download'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<download_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<download_id>[0-9]+)/results/$', views.results, name='results'),
-    url(r'^(?P<download_id>[0-9]+)/progress/$', views.progress, name='vote'),
+    url(r'^(?P<download_id>[0-9]+)/progress/$', views.progress, name='progress'),
 ]
--- download/views.py
+++ download/views.py
@@ -1,18 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Download
 
+
 def index(request):
     latest_download_list = Download.objects.order_by('-date')[:5]
-    output = ', '.join([d.link for d in latest_download_list])
-    return HttpResponse(output)
+    context = { 'latest_download_list': latest_download_list, }
+    return render(request, 'download/index.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at DOWNLOAD %s." % question_id)
+def detail(request, download_id):
+    download = get_object_or_404(Download, pk=download_id)
+    return render(request, 'download/detail.html', {'download': download})
 
-def results(request, question_id):
+def results(request, download_id):
     response = "You're looking at the results of DOWNLOAD %s."
-    return HttpResponse(response % question_id)
+    return HttpResponse(response % download_id)
 
-def progress(request, question_id):
-    return HttpResponse("You're processing DOWNLOAD %s." % question_id)
+def progress(request, download_id):
+    return HttpResponse("You're processing DOWNLOAD %s." % download_id)
