c64bdf07ca276c4b6337b5a8d3e4988910cefe96
torabi-ali <alitorabi2020@gmail.com>
2016-08-31 13:39:39 +0430

    try to make vies

--- begir/urls.py
+++ begir/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^download/', include('download.urls')),
+#    url('^register/', CreateView.as_view(template_name='register.html', form_class=UserCreationForm, success_url='/')),
+#    url('^accounts/', include('django.contrib.auth.urls')),
 ]
--- download/views.py
+++ download/views.py
@@ -1,21 +1,19 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-
-from .models import Download
+from django.shortcuts import render
 
 
 def index(request):
-    latest_download_list = Download.objects.order_by('-date')[:5]
-    context = { 'latest_download_list': latest_download_list, }
-    return render(request, 'download/index.html', context)
+    return render(request, 'download/index.html')
 
-def detail(request, download_id):
-    download = get_object_or_404(Download, pk=download_id)
-    return render(request, 'download/detail.html', {'download': download})
 
-def results(request, download_id):
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
     response = "You're looking at the results of DOWNLOAD %s."
-    return HttpResponse(response % download_id)
+    return HttpResponse(response % question_id)
 
-def progress(request, download_id):
-    return HttpResponse("You're processing DOWNLOAD %s." % download_id)
+
+def progress(request, question_id):
+    return HttpResponse("You're processing DOWNLOAD %s." % question_id)
